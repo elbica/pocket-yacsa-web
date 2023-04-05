89d6e3abcec3ec76f2c272e80863ab7f0592f1f7
elbica <sh0705k@gmail.com>
2023-04-05 20:20:41 +0900

    :tada: Init: 프로젝트 의존성 설치

---
+++ next.config.js
@@ -0,0 +1,40 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+
+    config.module.rules.push({
+      test: /.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgo: false,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    // Link: https://fe-developers.kakaoent.com/2022/220714-next-image/
+    imageSizes: [64, 256],
+    deviceSizes: [512],
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
+
+module.exports = nextConfig;
