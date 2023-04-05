b58452bddd8c7fa33825b05d89dc8f7ee9b6f77d
elbica <sh0705k@gmail.com>
2023-04-06 05:22:45 +0900

    :sparkles: Feat: 검색 기능에 필요한 hook 추가

--- src/hooks/common/index.ts
+++ src/hooks/common/index.ts
@@ -1,2 +1,7 @@
+export * from "./useColoredText";
+export * from "./useDebounce";
+export * from "./useInput";
 export * from "./useIsMount";
 export * from "./useIsomorphicLayoutEffect";
+export * from "./useLocalStorage";
+export * from "./useRecentSearch";
---
+++ src/hooks/common/useColoredText.tsx
@@ -0,0 +1,29 @@
+import type { ReactNode } from "react";
+
+interface Props {
+  tagName: string;
+  searchText: string;
+}
+
+export const useColoredText = ({ tagName, searchText }: Props) => {
+  const index = tagName.search(searchText); //문자 있는 지 없는 확인
+  const checkValidation = /([^가-힣a-z\x20])/i.test(searchText.replace("\\", ""));
+
+  const ColoredText: ReactNode = (
+    <>
+      {checkValidation && index !== -1 ? (
+        <div className="truncate">
+          <span>{tagName.slice(0, tagName.search(searchText))}</span>
+          <span className="text-secondary-1000">
+            {tagName.slice(index, index + searchText.length)}
+          </span>
+          <span>{tagName.slice(index + searchText.length)}</span>
+        </div>
+      ) : (
+        <span>{tagName}</span>
+      )}
+    </>
+  );
+
+  return { ColoredText };
+};
