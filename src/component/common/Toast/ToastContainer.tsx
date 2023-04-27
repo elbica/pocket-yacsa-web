ce6c2ac98e7159f3d12cb4164c84c84bc33aa8f3
elbica <sh0705k@gmail.com>
2023-04-28 00:40:39 +0900

    :sparkles: Feat: 토스트 디자인, 애니메이션 설정

--- src/component/common/Toast/ToastContainer.tsx
+++ src/component/common/Toast/ToastContainer.tsx
@@ -1,49 +1,23 @@
-import { useRef } from "react";
-
 import { Portal } from "@/component/common/Portal";
 import { Toast } from "@/component/common/Toast/Toast";
-import { useIsomorphicLayoutEffect } from "@/hooks/common";
 import { Z_INDEX } from "@/util/constant";
 
 import { useToastContext } from "./context";
 
-let prevHeight = 0;
-const EXPAND_DELAY = 200;
-
 export const ToastContainer = () => {
   const toasts = useToastContext();
 
-  const ref = useRef<HTMLElement>(null!);
-
-  /**
-   * NOTE FLIP Animation
-   *  @link https://web.dev/building-a-toast-component/#managing-one-or-many-toasts
-   */
-  useIsomorphicLayoutEffect(() => {
-    if (ref.current) {
-      const currentHeight = ref.current.getBoundingClientRect().height;
-
-      if (currentHeight > prevHeight) {
-        const invert = currentHeight - prevHeight;
-
-        ref.current.animate(
-          [{ transform: `translateY(${invert}px)` }, { transform: "translateY(0)" }],
-          {
-            duration: EXPAND_DELAY,
-            easing: "ease-out",
-          },
-        );
-      }
-      prevHeight = currentHeight;
-    }
-  }, [toasts]);
-
-  // TODO z-index 관리
   return (
     <Portal id="toast-portal">
+      <div
+        className={`pointer-events-none fixed h-screen bg-[rgba(23,23,23,0.35)] opacity-0 transition-opacity ${
+          toasts.length && "pointer-events-auto opacity-100"
+        } px-18 ${
+          Z_INDEX.toast
+        } m-auto grid w-full max-w-[44rem] place-items-center content-center gap-6`}
+      />
       <aside
-        className={`fixed inset-x-0 bottom-32 px-18 ${Z_INDEX.toast} m-auto grid w-full max-w-[44rem] place-items-center content-end gap-6`}
-        ref={ref}
+        className={`pointer-events-none fixed inset-0 px-18 ${Z_INDEX.toast} m-auto grid w-full max-w-[44rem] place-items-center content-center gap-6`}
       >
         {toasts.map((toast) => (
           <Toast key={toast.id} {...toast} />
